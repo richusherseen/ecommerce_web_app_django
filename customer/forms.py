c28134a0836fc4ac2dd58c453b1bd0b658dfd88c
richusherseen <richusherseen786@gmail.com>
2021-01-13 09:34:11 +0530

    user profile added and profile edit view added

--- customer/forms.py
+++ customer/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth.models import User
+from .models import Profile
 class LoginForm(forms.Form):
     
     username = forms.CharField(required=True,widget=forms.TextInput(attrs={'placeholder': 'User Name','class' : 'login-form'}))
@@ -12,3 +13,17 @@ class RegisterForm(forms.Form):
     password = forms.CharField(required=True,max_length=50, widget=forms.PasswordInput(attrs={'placeholder': 'password','class' : 'login-form'}))
     comfirm_password = forms.CharField(max_length=50,widget=forms.PasswordInput(attrs={'placeholder': 'confirm password','class' : 'login-form'}))
 
+class UserUpdateForm(forms.ModelForm):
+
+    
+    class Meta:
+
+        model = User
+        fields = ['username']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Profile
+        fields = ['image','name','email','phone','address']
--- customer/views.py
+++ customer/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView,View,CreateView,ListView
 from customer.models import ContactUs
-from customer.forms import RegisterForm,LoginForm
+from customer.forms import RegisterForm,LoginForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.models import User,auth
 from django.contrib.auth import login,authenticate,logout
 from product.models import *
+from .models import Profile
 from django.http import HttpResponse,JsonResponse
 import json
 
@@ -45,7 +46,7 @@ class HomeView(View):
 			user = User.objects.create(username = request.POST.get('username'))
 			user.set_password(password)
 			user.save()
-			return redirect('/home')
+			return redirect('profile')
 		
 		form = self.form_class(request.POST)
 		if form.is_valid():
@@ -86,39 +87,86 @@ class HomeView(View):
 			else:
 				print('not authenticated')
 
+#function for add to cart
 def updateItem(request):
-    data = json.loads(request.body)
-    print("giufdbgi",data)
-    productId = data['productId']
-    action = data['action']
-    print("Action:", action)
-    print("ProductId:", productId)
+	data = json.loads(request.body)
+	print("giufdbgi",data)
+	productId = data['productId']
+	action = data['action']
+	print("Action:", action)
+	print("ProductId:", productId)
 
-    customer=request.user
-    print("cud",customer)
-    product=ProductModel.objects.get(id=productId)
-    vendor=product.vendor
-    print("dealer",vendor)
-    order, created = Order.objects.get_or_create(customer=customer,complete=False)
-    order.vendor_id=vendor
-    order.save()
-    
-    orderItem, created = OrderItem.objects.get_or_create(order=order,product=product)
-
-    if action == 'add':
-        orderItem.quantity = (orderItem.quantity + 1)
-    elif action == 'remove':
-        orderItem.quantity = (orderItem.quantity - 1)
-
-    orderItem.save()
-
-    if orderItem.quantity <=0:
-        orderItem.delete()
-    
-
-
-    return JsonResponse('Item was added' , safe=False) 
+	customer=request.user
+	print("cud",customer)
+	product=ProductModel.objects.get(id=productId)
+	vendor=product.vendor
+	print("dealer",vendor)
+	order, created = Order.objects.get_or_create(customer=customer,complete=False)
+	order.vendor_id=vendor
+	order.save()
 	
+	orderItem, created = OrderItem.objects.get_or_create(order=order,product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <=0:
+		orderItem.delete()
+	
+
+
+	return JsonResponse('Item was added' , safe=False) 
+
+class ProfieView(View):
+	
+	def get(self,request):
+		
+		profiles = Profile.objects.get(user = request.user.id)
+		print('profilesssss',profiles)
+		items=[]
+		order ={'get_cart_total':0,'get_cart_items':0,'shipping':False}
+		cartItems=order['get_cart_items']
+
+		context={
+		'cartItems':cartItems,
+		'profiles':profiles
+		
+		}
+		return render(request,'profile.html',context)
+
+class EditProfile(View):
+
+	def get(self,request):
+		
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)
+
+		context={
+
+			'u_form': u_form,
+			'p_form': p_form
+		}
+
+		return render(request,'edit_profile.html',context)
+
+	def post(self,request):
+
+		u_form = UserUpdateForm(request.POST,instance=request.user)
+		p_form = ProfileUpdateForm(request.POST,request.FILES,
+		instance=request.user.profile)
+		print(p_form)
+		if u_form.is_valid() and p_form.is_valid():
+
+
+			u_form.save()
+			p_form.save()
+			
+			return redirect('profile')
+
 def contact(request):
 	if request.method == 'POST':
 		name = request.POST['name']
